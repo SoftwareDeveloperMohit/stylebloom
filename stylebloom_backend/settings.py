df7bef401865ec0f6fec07a50a8917135883eca9
mohit <patnimohit92@gmail.com>
2025-07-19 01:43:01 +0530

    Prepared project for Render deployment

--- stylebloom_backend/settings.py
+++ stylebloom_backend/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +14,8 @@ SECRET_KEY = 'django-insecure-ir=6@o4r$sh0q&sjm51ypq$y0&@9)g5ge1g)*g435@0*fc^ggi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', 'stylebloom.in']
+
 
 
 # Application definition
@@ -105,7 +107,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
